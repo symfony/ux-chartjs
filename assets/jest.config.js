948a9681195cd2403f6fab3aaac3268ae7c9dcda
Ryan Weaver <ryan@thatsquality.com>
2022-11-12 09:52:01 -0500

    [WIP] All Package directory restructure

--- assets/jest.config.js
+++ assets/jest.config.js
@@ -1,4 +1,4 @@
-const config = require('../../../../jest.config.js');
+const config = require('../../../jest.config.js');
 
 config.setupFilesAfterEnv.push('./test/setup.js');
 
--- src/ChartjsBundle.php
+++ src/ChartjsBundle.php
@@ -22,4 +22,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class ChartjsBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
