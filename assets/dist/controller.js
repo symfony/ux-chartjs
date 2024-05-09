symfony/ux

Fix Chart.js encore compilation

src/Chartjs/assets/dist/controller.js
@@ -1,6 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
-import Chart from 'chart.js/auto';
+import { Chart, registerables } from 'chart.js';
 
+Chart.register(...registerables);
 let isChartInitialized = false;
 class default_1 extends Controller {
     constructor() {

symfony/ux-chartjs

assets/dist/controller.js
@@ -1,6 +1,9 @@
 import { Controller } from '@hotwired/stimulus';
-import Chart from 'chart.js/auto';
+import { registerables, Chart } from 'chart.js';
 
+if (registerables != undefined) {
+    Chart.register(...registerables);
+}
 let isChartInitialized = false;
 class default_1 extends Controller {
     constructor() {